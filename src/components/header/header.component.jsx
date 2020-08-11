0580dce3bdadaa8ff8eab1fa207732786ab91de0
Azgar Hossain Shuvo <azgar.hossain@link3.net>
2020-08-11 17:49:13 +0600

    added redux and implemented with g=header and app component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { connect } from 'react-redux';
+
 import  { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
+
 
 const Header = ({ currentUser}) => (
     <div className='header'>
@@ -33,4 +36,7 @@ const Header = ({ currentUser}) => (
     </div>
 )
 
-export default Header;
+const mapStateToProps = (state) => ({
+    currentUser: state.user.currentUser
+});
+export default connect(mapStateToProps)(Header);
